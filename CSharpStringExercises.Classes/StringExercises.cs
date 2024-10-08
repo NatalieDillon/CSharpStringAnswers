b484df8a36e0e0e6c1caa415134a8ebd36d2e788
NatalieDillon <112569645+NatalieDillon@users.noreply.github.com>
2024-10-08 10:35:25 +0100

    Updated string exercises and calling code

--- CSharpStringExercises.Classes/StringExercises.cs
+++ CSharpStringExercises.Classes/StringExercises.cs
@@ -5,8 +5,153 @@ namespace CSharpStringExercises.Classes
 {
 	public class StringExercises
 	{
+		// 1. Mexican wave
+		// The input string will always be lower case but maybe empty.
+		// If the character in the string is whitespace then pass over it as if it was an empty seat
+		// Example
+		// Wave("hello") => [ "Hello", "hEllo", "heLlo", "helLo", "hellO"]
+		// There are already some unit tests for this function
+		public static List<string> Wave(string input)
+		{
+			List<string> wave = [];
+			for (int i = 0; i<input.Length; i++)
+			{
+				StringBuilder sb = new(input); // Easy to change a string using a string builder
+				if (!char.IsWhiteSpace(sb[i]))
+				{
+					sb[i] = char.ToUpper(sb[i]); // Can access the string builder by index
+					wave.Add(sb.ToString());
+				}				
+			}
+			return wave;
+		}
 
-		// 1: Pig Latin
+		// 2: Hello character codes - Convert to integer character codes
+		// Convert the word hello to its character codes (one integer for each code) and output them.
+		// Do this for hello in Chinese as well.
+		// In your output message you may wish to use nihao as 你好 may not print to the console
+		// Test this procedure by calling it from main
+		public static void WriteHelloCharacterCodes()
+		{
+			Write("Hello", "Hello");
+			Write("你好", "Nihao");
+		}
+
+		private static void Write(string input, string message)
+		{
+			Console.Write($"{message} is :");
+			foreach (char c in input)
+			{
+				Console.Write($"{(int)c} ");
+			}
+			Console.WriteLine();
+		}
+
+		// 3: Hello character codes - Convert to a byte array
+		// Convert the word hello to its character codes expressed as a byte array
+		// You can use Encoding.Default.GetBytes() for this
+		// Output the result
+		// Remember a byte is nothing but an integer between 0-255 that uses one byte of memory
+		// Do this for hello in Chinese as well
+		// In your output message you may wish to use nihao as 你好 may not print to the console
+		// What difference do you see with your answer to question 3?
+		// Test this procedure by calling it from main
+		public static void WriteHelloAsBytes()
+		{
+			string hello = "Hello";
+			byte[] helloBytes = Encoding.Default.GetBytes(hello);
+			Console.Write("Hello is :");
+			Console.WriteLine(string.Join(",", helloBytes));
+
+			string nihao = "你好";
+			byte[] nihaoBytes = Encoding.Default.GetBytes(nihao);
+			Console.Write("nihao is :");
+			Console.WriteLine(string.Join(",", nihaoBytes));
+
+			Console.WriteLine();
+		}
+
+
+		// 4. Character codes - convert integer character codes to a string
+		// Given an array of integers where each one represents a character code return the string
+		// Use a StringBuilder in case you get a long int array as input
+		// There are already some unit tests for this function
+		public static string ReadCharacterCodes(int[] codes)
+		{
+			StringBuilder sb = new();
+			foreach (char code in codes)
+			{
+				sb.Append(code);
+			}
+			return sb.ToString();
+		}
+
+		// 5. Character codes - convert character codes given as a byte array to a string
+		// Remember a byte is nothing but an integer between 0-255 that uses one byte of memory
+		// To test, the byte array: [ 206, 188, 206, 174, 206, 187, 206, 191 ] represents the string μήλο
+		// Write unit tests for this function
+		public static string ReadCharacterCodes(byte[] codes)
+		{			
+			return Encoding.Default.GetString(codes);
+		}
+
+		// 6. Anagram
+		// Given a word and a list of words return the number of words that are anagrams of the others
+		// Anagram("star", [ "rats", "arts", "arc" ]) => 2
+		// Write unit tests for this function
+		public static int Anagram(string input, string[] possibleAnagrams)
+		{
+			string sorted = SortString(input);
+			int count = 0;
+			foreach (string value in possibleAnagrams )
+			{
+				string sortedValue = SortString(value);
+				if (sorted.Equals(sortedValue))
+				{
+					count++;
+				}
+			}
+			return count;
+		}
+
+		private static string SortString(string input)
+		{
+			char[] charArray = input.ToLower().ToCharArray();
+			Array.Sort(charArray);
+			return new string(charArray);
+		}
+
+		// 7. Variable Name helper
+		public enum VariableNameType
+		{
+			CamelCase,
+			PascalCase,
+			SnakeCase
+		}
+		// Return the string in either camelCase, PascalCase or snake_case depending on the user choice
+		// You can assume the input is currently written in the normal way with spaces between words
+		// E.g Cost of cakes => costOfCakes, CostOfCakes or cost_of_cakes depending on the option chosen
+		// Write unit tests for this function
+		public static string WriteVariableName(string input, VariableNameType caseNeeded = VariableNameType.CamelCase)
+		{
+			string[] words = input.Split();
+			for (int i = 0; i < words.Length; i++)
+			{
+				words[i] = words[i].ToLower();
+				if ((i > 0 && caseNeeded == VariableNameType.CamelCase) || caseNeeded == VariableNameType.PascalCase)
+				{
+					// make first letter a capital
+					StringBuilder newWord = new(words[i]);
+					newWord[0] = char.ToUpper(newWord[0]);
+					words[i] = newWord.ToString();
+				}
+			}
+			string delimiter = caseNeeded == VariableNameType.SnakeCase ? "_" : string.Empty;
+			return string.Join(delimiter, words);
+		}
+
+
+		// 8: Pig Latin
 		// Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
 		// Leave punctuation marks untouched.
 		// If any words entirely consist of punctuation marks you should leave them unchanged
@@ -16,7 +161,7 @@ namespace CSharpStringExercises.Classes
 		public static string PigLatin(string input)
 		{
 			string[] words = input.Split(' ');
-			for (int i = 0; i< words.Length; i++)
+			for (int i = 0; i < words.Length; i++)
 			{
 				int wordEnd = FindLastIndexNotPunctuation(words[i]);
 				int firstLetter = FindFirstIndexNotPunctuation(words[i]);
@@ -53,155 +198,6 @@ namespace CSharpStringExercises.Classes
 			}
 			return -1;
 		}
-
-		// 2. Mexican wave
-		// The input string will always be lower case but maybe empty.
-		// If the character in the string is whitespace then pass over it as if it was an empty seat
-		// Example
-		// Wave("hello") => { "Hello", "hEllo", "heLlo", "helLo", "hellO"}
-		// Write some unit tests for this function
-		public static List<string> Wave(string input)
-		{
-			List<string> wave = new List<string>();
-			for (int i = 0; i<input.Length; i++)
-			{
-				StringBuilder sb = new(input); // Easy to change a string using a string builder
-				if (!char.IsWhiteSpace(sb[i]))
-				{
-					sb[i] = char.ToUpper(sb[i]); // Can access the string builder by index
-					wave.Add(sb.ToString());
-				}				
-			}
-			return wave;
-		}
-
-		// 3: Hello character codes - Convert to integer character codes
-		// Convert the word hello to its character codes (one integer for each code) and output them.
-		// Do this for hello in Chinese as well
-		// Test this procedure by calling it from main
-		public static void WriteHelloCharacterCodes()
-		{
-			Write("Hello", "Hello");
-			Write("你好", "Nihao");
-		}
-
-		private static void Write(string input, string message)
-		{
-			Console.Write($"{message} is :");
-			foreach (char c in input)
-			{
-				Console.Write($"{(int)c} ");
-			}
-			Console.WriteLine();
-		}
-
-		// 4: Hello character codes - Convert to a byte array
-		// Convert the word hello to its character codes expressed as a byte array
-		// You can use Encoding.Default.GetBytes() for this
-		// Output the result
-		// Remember a byte is nothing but an integer between 0-255 that uses one byte of memory
-		// Do this for hello in Chinese as well
-		// What difference do you see with your answer to question 3?
-		// Test this procedure by calling it from main
-		public static void WriteHelloAsBytes()
-		{
-			Console.WriteLine(Encoding.Default);
-
-			string hello = "Hello";
-			byte[] helloBytes = Encoding.Default.GetBytes(hello);
-			Console.Write("Hello is :");
-			Console.WriteLine(string.Join(",", helloBytes));
-
-			string nihao = "你好";
-			byte[] nihaoBytes = Encoding.Default.GetBytes(nihao);
-			Console.Write("Nihao is :");
-			Console.WriteLine(string.Join(",", nihaoBytes));
-
-			Console.WriteLine();
-		}
-
-
-		// 5. Character codes - convert integer character codes to a string
-		// Given an array of integers where each one represents a character code return the string
-		// To test, the int array { 956 942 955 959 } represents the string μήλο
-		// It might not print nicely to the console but you can inspect the variable, use an assert, or write a test
-		// Use a StringBuilder in case you get a long int array as input
-		// Write unit tests for this function
-		public static string ReadCharacterCodes(int[] codes)
-		{
-			StringBuilder sb = new();
-			foreach (char code in codes)
-			{
-				sb.Append(code);
-			}
-			return sb.ToString();
-		}
-
-		// 6. Character codes - convert character codes given as a byte array to a string
-		// Remember a byte is nothing but an integer between 0-255 that uses one byte of memory
-		// To test, the byte array: { 206, 188, 206, 174, 206, 187, 206, 191 } represents the string μήλο
-		// It might not print nicely to the console but you can inspect the variable, use an assert or write a test
-		// Write unit tests for this function
-		public static string ReadCharacterCodes(byte[] codes)
-		{			
-			return Encoding.Default.GetString(codes);
-		}
-
-		// 7. Anagram
-		// Given a word and a list of words return the number of words that are anagrams of the others
-		// Anagram("star", new string[] { "rats", "arts", "arc" }) => 2
-		// Write unit tests for this function
-		public static int Anagram(string input, string[] possibleAnagrams)
-		{
-			string sorted = SortString(input);
-			int count = 0;
-			foreach (string value in possibleAnagrams )
-			{
-				string sortedValue = SortString(value);
-				if (sorted.Equals(sortedValue))
-				{
-					count++;
-				}
-			}
-			return count;
-		}
-
-		private static string SortString(string input)
-		{
-			char[] charArray = input.ToLower().ToCharArray();
-			Array.Sort(charArray);
-			return new string(charArray);
-		}
-
-		// 8. Variable Name helper
-		public enum VariableNameType
-		{
-			CamelCase,
-			PascalCase,
-			SnakeCase
-		}
-		// Return the string in either camelCase, PascalCase or snake_case depending on the user choice
-		// You can assume the input is currently written in the normal way with spaces between words
-		// E.g Cost of cakes => costOfCakes, CostOfCakes or cost_of_cakes depending on the option chosen
-		// Write unit tests for this function
-		public static string WriteVariableName(string input, VariableNameType caseNeeded = VariableNameType.CamelCase)
-		{
-			string[] words = input.Split();
-			for (int i = 0; i < words.Length; i++)
-			{
-				words[i] = words[i].ToLower();
-				if ((i > 0 && caseNeeded == VariableNameType.CamelCase) || caseNeeded == VariableNameType.PascalCase)
-				{
-					// make first letter a capital
-					StringBuilder newWord = new(words[i]);
-					newWord[0] = char.ToUpper(newWord[0]);
-					words[i] = newWord.ToString();
-				}
-			}
-			string delimiter = caseNeeded == VariableNameType.SnakeCase ? "_" : string.Empty;
-			return string.Join(delimiter, words);
-		}
-
 
 		// 9: TV Remote (Extension)
 		// Consider the search screen pictured below
--- CSharpStringExercises/Program.cs
+++ CSharpStringExercises/Program.cs
@@ -9,31 +9,52 @@ namespace CSharpStringExercises
 		static void Main()
 		{
 			// Add your calling code here
-			// This is essential to demonstrate you have tested Q3 and Q4
-			// You will be writing your own tests for the others.
-			// However it may be useful to test them here as you develop them 
+			// This is essential to demonstrate you have tested Q2 and Q3
+			// Tests have already be written for Q1, Q4 and Q8
+			// You will need to write your own tests for Q5, Q6, Q7 and the extension.
+			// However it may be useful to test all your subprograms from here as you develop them 
 			
-			Console.WriteLine(StringExercises.PigLatin("&,%%!") + "\n");
+			
+			Console.OutputEncoding = Encoding.Unicode; // Allows greek letters to be output but not chinese characters
 
-            Console.WriteLine(StringExercises.PigLatin("I don't like Mondays much.") + "\n");
+			Console.WriteLine("Q1:");
+			Console.WriteLine(string.Join(",", StringExercises.Wave("hello world")) + "\n");
 
-            Console.WriteLine(string.Join(",", StringExercises.Wave("hello world")) + "\n");
-
-			Console.WriteLine(StringExercises.Anagram("star", new string[] { "rats", "arts", "arc" }));
-
+			Console.WriteLine("Q2:");
 			StringExercises.WriteHelloCharacterCodes();
+			Console.WriteLine();
+
+			Console.WriteLine("Q3:");
 			StringExercises.WriteHelloAsBytes();
 
-			byte[] code = new byte[] { 206, 188, 206, 174, 206, 187, 206, 191 };
-			Console.WriteLine(StringExercises.ReadCharacterCodes(code) + "\n");
+			Console.WriteLine("Q4:");
+			int[] intCodes =[ 956, 942, 955, 959 ];
+			string intCodesToString = StringExercises.ReadCharacterCodes(intCodes);
+ 			Console.WriteLine($"[ 956, 942, 955, 959 ] => {intCodesToString}\n");
 
-			int[] intCodes = new int[] { 956, 942, 955, 959 };
-			Console.WriteLine(StringExercises.ReadCharacterCodes(intCodes) + "\n");
+			Console.WriteLine("Q5:");
+			byte[] codes = [ 206, 188, 206, 174, 206, 187, 206, 191 ];
+			string byteCodesToString = StringExercises.ReadCharacterCodes(codes);
+			Console.WriteLine($"[ 206, 188, 206, 174, 206, 187, 206, 191 ] => {byteCodesToString}\n");
 
-			string variableName = StringExercises.WriteVariableName("Some silly variable name", StringExercises.VariableNameType.CamelCase);
-            string variableName2 = StringExercises.WriteVariableName("Some silly variable name", StringExercises.VariableNameType.SnakeCase);
-            Console.WriteLine(variableName);
-            Console.WriteLine(variableName2);
+			Console.WriteLine("Q6:");
+			int numAnagrams = StringExercises.Anagram("star", ["rats", "arts", "arc"]);
+			Console.WriteLine($"[rats, arts, arc] contains {numAnagrams} of star\n");
+
+			Console.WriteLine("Q7");
+			string input = "Happy Monday";
+			string snakeCase = StringExercises.WriteVariableName(input, StringExercises.VariableNameType.SnakeCase);
+			string camelCase = StringExercises.WriteVariableName(input);
+			string pascalCase = StringExercises.WriteVariableName(input, StringExercises.VariableNameType.PascalCase);
+			Console.WriteLine($"{input} => {snakeCase} {camelCase} {pascalCase}\n");
+
+			Console.WriteLine("Q8:");
+			Console.WriteLine(StringExercises.PigLatin("&,%%!"));
+            Console.WriteLine(StringExercises.PigLatin("I don't like Mondays much.") + "\n");
+
+			Console.WriteLine("Q9:");
+			int keyPresses = StringExercises.CalculateKeyPresses(["War", "and", "Peace"]);
+			Console.WriteLine($"War and Peace requires {keyPresses} key presses");
         }
 
 	}
